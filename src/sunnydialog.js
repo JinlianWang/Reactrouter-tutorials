d54ba7324e57440a2a02dd23aed2d6813c60f3f3
Sunny Wang <wangjinlian@hotmail.com>
2019-04-27 21:34:12 -0400

    Add more attributes for dialog for desirable behaviors.

--- src/sunnydialog.js
+++ src/sunnydialog.js
@@ -5,7 +5,7 @@ import Popup from "reactjs-popup";
 class SunnyDialog extends React.Component {
   render() {
     return (
-	  <Popup trigger={<button> Trigger</button>} position="right center">
+	  <Popup trigger={<button> Trigger</button>} position="right center" modal={true} closeOnDocumentClick={false}>
 		<div>Popup content here !!</div>
 		<form>
 			<input placeholder="name" type="name" />
