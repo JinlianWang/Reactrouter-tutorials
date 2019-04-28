bbe16cb9c6070f65711f9851eb1de4fea8e6dc64
Sunny Wang <wangjinlian@hotmail.com>
2019-04-27 21:17:09 -0400

    Added reactjs-popup with a simple example.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import SunnyDialog from "./sunnydialog";
 
 class App extends React.Component {
   render() {
     return (
       <div>
        <h1>Home</h1>
+       <SunnyDialog> </SunnyDialog>
       </div>
     );
   }
---
+++ src/sunnydialog.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Popup from "reactjs-popup";
+
+
+class SunnyDialog extends React.Component {
+  render() {
+    return (
+	  <Popup trigger={<button> Trigger</button>} position="right center">
+		<div>Popup content here !!</div>
+		<form>
+			<input placeholder="name" type="name" />
+			<input placeholder="name" type="email" />
+			<button onClick={this.onSubmit}>Submit</button>
+      	</form>
+	  </Popup>
+    );
+  }
+}
+
+export default SunnyDialog;
