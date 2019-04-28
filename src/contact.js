646169c256ef920de4fa24911a6b7b3c5b444c4b
Sunny Wang <wangjinlian@hotmail.com>
2019-04-27 20:13:58 -0400

    Make onSubmit working.

--- src/contact.js
+++ src/contact.js
@@ -1,14 +1,23 @@
 import React from "react";
 
 class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
 
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+  
+  onSubmit(event) {
+    this.props.history.push("/");
+  };
 
   render() {
     return (
       <form>
         <input placeholder="name" type="name" />
         <input placeholder="name" type="email" />
-        <button >Submit</button>
+        <button onClick={this.onSubmit}>Submit</button>
       </form>
     );
   }
